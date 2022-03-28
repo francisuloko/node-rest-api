7c72ea3520340fa2561c5a72951c88c805654d5f
francisuloko <francisuloko@yahoo.com>
2022-03-28 23:16:16 +0100

    Add middlewares

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
   console.log("Connected to MongoDB");
 });
 
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/", (res, req) => {
+  req.send("Welcome to node rest api");
+});
+
 app.listen(8800, () => {
   console.log("Server is running...");
 });
