bcdb3bd1600af188cf66ad7566d3ec8b0b6167f8
francisuloko <francisuloko@yahoo.com>
2022-03-28 23:36:39 +0100

    Create user route

--- index.js
+++ index.js
@@ -4,15 +4,22 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const userRoute = require("./routes/users");
 
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
   console.log("Connected to MongoDB");
 });
 
+
+// Middleware
+
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
+
+app.use("/api/users", userRoute);
 
 app.listen(8800, () => {
   console.log("Server is running...");
