9647a5eedac89c202f054d208b842851838ac430
francisuloko <francisuloko@yahoo.com>
2022-03-28 23:09:58 +0100

    Add connection to MongoDB

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 
 dotenv.config();
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
+  console.log("Connected to MongoDB");
+});
 
 app.listen(8800, () => {
   console.log("Server is running...");
