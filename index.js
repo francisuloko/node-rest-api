be3c4d8e80a7b73824aeaf6d5475235f9f6a7e62
francisuloko <francisuloko@yahoo.com>
2022-03-28 22:29:01 +0100

    Require dependencies

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+
+app.listen(8800, () => {
+  console.log("Server is running...");
+});
