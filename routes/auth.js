ec0644b56ef98edb0c2e3f176b4c589d2ddf3f12
francisuloko <francisuloko@yahoo.com>
2022-03-29 01:24:27 +0100

    Add login action

--- routes/auth.js
+++ routes/auth.js
@@ -3,14 +3,14 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
 
-// Register user
+// Register User
 router.post('/register', async (res, req) => {
   try {
     const salt = await bcrypt.getSalt(10);
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
     const newUser =  await new User({
-      username: res.body.username,
-      email: res.body.email,
+      username: req.body.username,
+      email: req.body.email,
       password: hashedPassword,
     });
     const user = await newUser.save();
@@ -20,4 +20,20 @@ router.post('/register', async (res, req) => {
   }
 });
 
+// Login User
+router.post('/login', async (res, req) => {
+  
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json("User not fount.");
+
+    const password = await bcrypt.compare(req.body.password, user.password);
+    !password && res.status(400).json("Password not valid.");
+
+    res.status(200).json(user);
+
+  } catch(err) {
+    return err;
+  }
+});
 module.exports = router;
