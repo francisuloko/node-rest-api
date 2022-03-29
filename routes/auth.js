8bee4fbf2265a6dc5a431e305b74d4d4c2a11698
francisuloko <francisuloko@yahoo.com>
2022-03-29 01:05:11 +0100

    Add user registration route

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
+const User = require("../models/User");
 
-router.get('/', (res, req) => {
-  req.send('Auth page.');
+
+// Register user
+router.post('/register', async (res, req) => {
+  const newUser =  await new User({
+    username: res.body.username,
+    email: res.body.email,
+    password: res.body.password,
+  });
+
+  try {
+    const user = await newUser.save();
+    req.status(200).json(user);
+  } catch(err) {
+    return err;
+  }
 });
 
 module.exports = router;
