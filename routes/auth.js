d132442c86a7fd60389acf97db94a40e83bab853
francisuloko <francisuloko@yahoo.com>
2022-03-29 01:11:26 +0100

    Add hashed password to user model

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,18 @@
 const router = require('express').Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 
 // Register user
 router.post('/register', async (res, req) => {
-  const newUser =  await new User({
-    username: res.body.username,
-    email: res.body.email,
-    password: res.body.password,
-  });
-
   try {
+    const salt = await bcrypt.getSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    const newUser =  await new User({
+      username: res.body.username,
+      email: res.body.email,
+      password: hashedPassword,
+    });
     const user = await newUser.save();
     req.status(200).json(user);
   } catch(err) {
