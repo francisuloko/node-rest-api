cf26965b1ad0066f05e8fbe69c8a46b29dc67ebd
francisuloko <francisuloko@yahoo.com>
2022-03-30 10:47:11 +0100

    Remove unused lines of code

--- routes/auth.js
+++ routes/auth.js
@@ -1,38 +1,35 @@
 const router = require('express').Router();
-const User = require("../models/User");
-const bcrypt = require("bcrypt");
-
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
 
 // Register User
 router.post('/register', async (res, req) => {
   try {
     const salt = await bcrypt.getSalt(10);
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
-    const newUser =  await new User({
+    const newUser = await new User({
       username: req.body.username,
       email: req.body.email,
       password: hashedPassword,
     });
     const user = await newUser.save();
     res.status(200).json(user);
-  } catch(err) {
+  } catch (err) {
     res.status(500).json(err);
   }
 });
 
 // Login User
 router.post('/login', async (res, req) => {
-  
   try {
     const user = await User.findOne({ email: req.body.email });
-    !user && res.status(404).json("User not fount.");
+    !user && res.status(404).json('User not fount.');
 
     const password = await bcrypt.compare(req.body.password, user.password);
-    !password && res.status(400).json("Password not valid.");
+    !password && res.status(400).json('Password not valid.');
 
     res.status(200).json(user);
-
-  } catch(err) {
+  } catch (err) {
     res.status(500).json(err);
   }
 });
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,3 @@
 const router = require('express').Router();
 
-router.get('/', (res, req) => {
-  res.send('Users page');
-});
-
 module.exports = router;
