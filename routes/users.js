9e4ef7465279fb242eb3843aa5a8c416b59fd17e
francisuloko <francisuloko@yahoo.com>
2022-03-30 13:59:18 +0100

    Add unfollow user endpoint

--- routes/users.js
+++ routes/users.js
@@ -74,4 +74,26 @@ router.put('/:id/follow', async (req, res) => {
   }
 });
 
+// Unfollow
+
+router.put('/:id/unfollow', async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if(user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $pull: { followers: req.body.userId }});
+        await currentUser.updateOne({ $pull: { followings: req.params.id }});
+        res.status(200).json("You unfollowed user.")
+      } else {
+        res.status(403).json("You are not following")
+      }
+    } catch(err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("Can't unfollow self.")
+  }
+});
+
 module.exports = router;
