ace4548b57997f9ec68235cadbb7aebc14e5645e
francisuloko <francisuloko@yahoo.com>
2022-03-31 22:45:47 +0100

    Config post route

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const userRoute = require('./routes/users');
 const authRoute = require('./routes/auth');
+const postRoute = require('./routes/posts');
 
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
@@ -21,6 +22,8 @@ app.use(morgan('common'));
 
 app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
+app.use('/api/posts', postRoute);
+
 
 app.listen(8800, () => {
   // console.log('Server is running...');
