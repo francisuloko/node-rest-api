2a2aba9e22ac225c688041f6fef4e621d2390836
francisuloko <francisuloko@yahoo.com>
2022-04-06 08:29:23 +0100

    Add update post endpoint

--- routes/posts.js
+++ routes/posts.js
@@ -13,4 +13,19 @@ router.post('/', async (req, res) => {
   }
 });
 
+// Update
+router.put('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json('Updated successfully.');
+    } else {
+      res.status(403).json('Not allowed.');
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
